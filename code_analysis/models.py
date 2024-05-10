3cd741958a6820dbfd59ca3f30dc9062b64b3135
Shahzaib Nasir <shahzaibnasir550@gmail.com>
2024-05-10 06:19:24 -0700

    changes

--- code_analysis/models.py
+++ code_analysis/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from users.models import UserProfile 
+from users.models import UserProfile
 
 class CodeSnippet(models.Model):
     code_snippet = models.TextField()
--- users/models.py
+++ users/models.py
@@ -1,15 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth import get_user_model
-User=get_user_model()
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class UserProfile(AbstractUser):
+    student_id = models.CharField(max_length=20, unique=True)
+   
 
     def __str__(self):
-        return self.user.username
-
-# from django.contrib.auth.models import User
-class CodeSnippetImage(models.Model):
-    image = models.BinaryField()
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+        return self.username
